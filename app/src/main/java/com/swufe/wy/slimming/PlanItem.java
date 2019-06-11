021c5494bf1318cce4fcc36d750a3e7fabe78903
Your Name <913105698@qq.com>
2019-06-11 21:09:48 +0800

    新增计划编辑列表的配置文件edit_list.xml、数据库访问对象DBHelper.java、创建数据列表的实体对象PlanItem.java

---
+++ app/src/main/java/com/swufe/wy/slimming/PlanItem.java
@@ -0,0 +1,53 @@
+package com.swufe.wy.slimming;
+
+public class PlanItem {
+
+    private int id;
+    private String planTitle;
+    private String planContent;
+    private int finishTimes;
+
+    public PlanItem() {
+        super();
+        planTitle = "";
+        planContent = "";
+        finishTimes = 0;
+    }
+
+    public PlanItem(String planTitle, String planContent, int times) {
+        super();
+        this.planTitle = planTitle;
+        this.planContent = planContent;
+        this.finishTimes = times;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPlanTitle() {
+        return planTitle;
+    }
+    public void setPlanTitle(String planTitle) {
+        this.planTitle = planTitle;
+    }
+
+    public String planContent() {
+        return planContent;
+    }
+    public void planContent(String planContent) {
+        this.planContent = planContent;
+    }
+
+    public int getFinishTimes() {
+        return finishTimes;
+    }
+
+    public void setFinishTimes(int finishTimes) {
+        this.finishTimes = finishTimes;
+    }
+}
